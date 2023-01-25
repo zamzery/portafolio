b2e59b5963d8dba2ef1aaa11764e837e48e6392f
Eduardo Martínez <zamzery@gmail.com>
2023-01-25 14:51:16 -0600

    Seguimiento de portafolio

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,34 @@
 import './index.scss'
-// import LogoTitle from '../../assets/images/logo-s.png';
 import {Link} from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
+import {useEffect, useState} from 'react';
+import Logo from '../Logo';
 
 const Home = () => {
+	const [ letterClass, setLetterClass ] = useState( 'text-animate' );
+	const nameArray = [ 'E', 'd', 'u', 'a', 'r', 'd', 'o', ' ', 'M', 'a', 'r', 't', 'í', 'n', 'e', 'z', '.', ];
+	const jobArray = [ 'w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.', ];
+
+	useEffect( () => {
+		setTimeout( () => {
+			setLetterClass( 'text-animate-hover' )
+		}, 4000 )
+	}, [] );
+
 	return (
 		<div className='container home-page'>
 			<div className='text-zone'>
 				<h1><br />
 					Hello, <br />
-					I'm Eduardo Martínez <br />
-					web developer<br />
-					{/* <img src={LogoTitle} alt="developer" /> */}
+					<AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={10} /><br />
+					<AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={16} /><br />
 				</h1>
 				<h2>
 					Fullstack Developer / Graphic Designer
 				</h2>
 				<Link to="/contact" className='flat-button'>CONTACT ME</Link>
 			</div>
+			<Logo />
 		</div>
 	)
 }
---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Logo = () => {
+	return (
+		<div className='logo-container'>
+
+		</div>
+	)
+}
+
+export default Logo
